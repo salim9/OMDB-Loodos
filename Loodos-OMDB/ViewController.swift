bb7a15578439583971a3bfb72cf2122379ba3cb8
Salim Uzun <salim.uzun@obss.com.tr>
2021-08-26 02:15:17 +0300

    Initial Commit

---
+++ Loodos-OMDB/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Loodos-OMDB
+//
+//  Created by Salim Uzun on 26.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
